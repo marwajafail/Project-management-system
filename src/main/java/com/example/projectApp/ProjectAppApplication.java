2e94a5aa45d3653a0476aa84c1bc4c2c22d0c41a
marwa jafail <marwa.a.jafail@gmail.com>
2024-11-13 23:15:27 +0300

    first commit

---
+++ src/main/java/com/example/projectApp/ProjectAppApplication.java
@@ -0,0 +1,25 @@
+package com.example.projectApp;
+
+import com.example.projectApp.storage.StorageProperties;
+import com.example.projectApp.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class ProjectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAppApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
---
+++ src/test/java/com/example/projectApp/ProjectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
