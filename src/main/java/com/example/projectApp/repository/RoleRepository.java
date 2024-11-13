fabe686688c41135443f6725183db2c8724e3271
marwa jafail <marwa.a.jafail@gmail.com>
2024-11-13 23:23:02 +0300

    add RoleRepository

---
+++ src/main/java/com/example/projectApp/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.projectApp.repository;
+
+import com.example.projectApp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
