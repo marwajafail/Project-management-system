329b885e5413b0dee8a4e8d66dd63a12603ba6bb
marwa jafail <marwa.a.jafail@gmail.com>
2024-11-13 23:17:32 +0300

    add Role model

---
+++ src/main/java/com/example/projectApp/model/Role.java
@@ -0,0 +1,41 @@
+package com.example.projectApp.model;
+
+import com.example.projectApp.Dto.RoleDto;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "\"roleId\"", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    private List<User> users;
+
+    public Role(RoleDto dto) {
+        this.id = dto.getId();
+        this.name = dto.getName();
+        this.description = dto.getDescription();
+        this.users = dto.getUsers() != null ?
+                dto.getUsers().stream().map(User::new).toList() :
+                null;
+
+    }
+}
